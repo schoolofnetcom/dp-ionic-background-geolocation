bcdda93584f482e9bb3722cca0167ccdbae015a1
Erik Figueiredo <erik.figueiredo@gmail.com>
2018-10-01 02:32:20 -0300

    final

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BackgroundGeolocationConfig, BackgroundGeolocation, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private backgroundGeolocation: BackgroundGeolocation) {
 
+  }
+
+  ionViewDidLoad() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    const config: BackgroundGeolocationConfig = {
+      desiredAccuracy: 10,
+      stationaryRadius: 20,
+      distanceFilter: 30,
+      interval: 5000,
+      debug: true,
+      stopOnTerminate: false,
+    }
+
+    this.backgroundGeolocation.configure(config)
+      .subscribe((location: BackgroundGeolocationResponse) => {
+        console.log(location);
+      });
+
+    this.backgroundGeolocation.start();
+    // this.backgroundGeolocation.sttop();
   }
 
 }
